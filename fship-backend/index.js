677fd1391846adadef4f397fdd948f2aaca3eb88
firtysh <sm2450@it.jgec.ac.in>
2023-02-27 01:28:52 +0530

    delete

--- fship-backend/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 5000;
-
-app.get('/', (req, res) => res.send('Hello World!'));
-
-app.listen(port, () => console.log(`app listening on port ${port}!`));
